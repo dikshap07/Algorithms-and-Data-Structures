b32d99e9593597ec8db413e889281432287041e5
Diksha Paliwal <51503505+dikshap07@users.noreply.github.com>
2022-09-26 11:39:51 -0400

    Add files via upload

--- KSortedArray.py
+++ KSortedArray.py
@@ -11,9 +11,6 @@ def KSortedArray(nums,k):
 
         heapq.heappush(my_heap,nums[i]) #adding element to heap
 
-        #heapify
-        heapq.heapify(my_heap)
-
         #if size of heap > k : we pop
 
 
@@ -22,7 +19,9 @@ def KSortedArray(nums,k):
             sorted_nums.append(heapq.heappop(my_heap))
 
     
+    while len(my_heap) != 0:
 
+        sorted_nums.append(heapq.heappop(my_heap))
         
     return sorted_nums
 
@@ -38,3 +37,29 @@ print(KSortedArray([6,5,3,2,8,10,9],3))
 
 
 
+
+
+def KSortedArray_(nums,k):
+
+    sorted_nums = []
+
+    my_heap = []
+
+
+    for i in range(len(nums)):
+
+        heapq.heappush(my_heap,nums[i]) #adding element to heap
+
+        #if size of heap > k : we pop
+
+
+        if len(my_heap) > k :
+
+            sorted_nums.append(heapq.heappop(my_heap))
+
+    
+    sorted_nums += my_heap
+        
+    return sorted_nums
+
+print(KSortedArray_([6,5,3,2,8,10,9],3))
