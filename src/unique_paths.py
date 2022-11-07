a2dfe08328b4b1a70f9dcee87563f37f40dbc9c4
dpaliwa <dpaliwa@ncsu.edu>
2022-11-07 16:25:52 -0500

    added unique paths recursion problem

---
+++ src/unique_paths.py
@@ -0,0 +1,18 @@
+
+
+'''given (n,m) return the num of unique ways you can reach from left top corner to bottom right corner
+constraints: can only move down or right at one point'''
+
+
+def unique_paths(n,m):
+
+    if n == 1 or m == 1:
+
+        return 1
+
+    else:
+        return unique_paths(n-1,m) + unique_paths(n,m-1)
+
+
+print(unique_paths(2,4))
+
