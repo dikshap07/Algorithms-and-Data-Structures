4bfc8663ed9849b2be34f973ea7160c27050a3e3
dpaliwa <dpaliwa@ncsu.edu>
2022-11-16 15:06:48 -0500

    added isMajorityElement.py

---
+++ src/isMajorityElement.py
@@ -0,0 +1,38 @@
+ def isMajorityElement(nums: List[int], target: int) -> bool:
+    total_len = len(nums)
+
+    def binarySearch(arr,target):
+
+        s = 0
+        e = len(arr)
+
+        while s < e:
+
+            m = (s+e)//2
+
+            if arr[m] == target:
+
+                return m
+            
+            elif arr[m] < target:
+                s = m+1
+
+            else:
+                e = m
+        return -1
+
+    freq = 0
+    found_index = 0
+
+    while found_index!=-1:
+
+        found_index = binarySearch(nums,target)
+        if found_index != -1:
+
+            freq+=1
+            nums.pop(found_index)
+
+    print("freq",freq)
+    print("majority",len(nums)/2)
+
+    return freq > (total_len/2)
